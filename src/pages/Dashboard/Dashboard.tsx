747349628200bbb56bf09c00205af31b2fb6bee0
Serhiy <serhiy.mos@gmail.com>
2023-11-15 13:19:56 +0400

    Added filtering

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,10 +2,28 @@ import { useEffect, useState } from 'react';
 import { getEmployees } from '../../services/employee.service';
 import { Employee } from '../../interfaces/employee';
 import { EmployeeTable } from '../../components/EmployeeTable/EmployeeTable';
+import TextInput from '../../components/TextInput/TextInput';
+import './Dashboard.scss'
+
+const debounceTimeout = 500;
+const debounce = <F extends (...args: any[]) => any>(
+  callback: F,
+  waitFor: number,
+) => {
+  let timeout: number = 0
+
+  const debounced = (...args: any[]) => {
+    clearTimeout(timeout)
+    // @ts-ignore
+    timeout = setTimeout(() => callback(...args), waitFor)
+  }
+
+  return debounced;
+}
 
 export const Dashboard = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
-  // const [query, setQuery] = useState();
+  const [loading, setLoading] = useState(true);
   const [filterColumn, setFilterColumn] = useState('');
   const [filterColumnAsc, setFilterColumnAsc] = useState(true);
 
@@ -17,11 +35,14 @@ export const Dashboard = () => {
       })
       .catch(() => {
         // TODO: Error handling implementation here
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
   useEffect(() => {
-    // Simulate front-end filtering, however if data is huge we would definitely shift it to back-end
+    // Simulate front-end filtering, however if dataset is huge we would definitely shift it to back-end
     employees.sort((a: Employee, b: Employee) => {
       const aProperty = a[filterColumn];
       const bProperty = b[filterColumn];
@@ -37,6 +58,22 @@ export const Dashboard = () => {
     setEmployees([...employees.reverse()]);
   }, [filterColumnAsc]);
 
+  const getEmployeesByQuery = (value: string) => {
+    setLoading(true);
+
+    getEmployees(value)
+      .then(({ data }) => {
+        setEmployees(data);
+      })
+      .catch(() => {
+        // TODO: Error handling implementation here
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleQueryChange = debounce(getEmployeesByQuery, debounceTimeout);
   const handleColumnClick = (columnName: string) => {
     if (filterColumn === columnName) {
       setFilterColumnAsc(!filterColumnAsc); // Invert order
@@ -48,7 +85,10 @@ export const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      <EmployeeTable data={employees} handleColumnClick={handleColumnClick} />
+      <div className="dashboard__filters-container">
+        <TextInput fullWidth handleChange={handleQueryChange} name={'table-query'} label="Filter query" />
+      </div>
+      {loading ? <p>Loading...</p> : <EmployeeTable data={employees} handleColumnClick={handleColumnClick} />}
     </div>
   );
 }
