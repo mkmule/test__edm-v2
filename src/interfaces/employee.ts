6223fc38c09369d13e738baa6e8f3867ddfecc8b
Serhiy <serhiy.mos@gmail.com>
2023-11-15 13:03:24 +0400

    Added sorting by column, asc and desc

--- src/interfaces/employee.ts
+++ src/interfaces/employee.ts
@@ -1,4 +1,6 @@
 export interface Employee {
+  [key: string]: any;
+
   company: string;
   contact: string;
   country: string;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,11 +6,13 @@ import { EmployeeTable } from '../../components/EmployeeTable/EmployeeTable';
 export const Dashboard = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
   // const [query, setQuery] = useState();
+  const [filterColumn, setFilterColumn] = useState('');
+  const [filterColumnAsc, setFilterColumnAsc] = useState(true);
 
   useEffect(() => {
+    // Initial data fetch
     getEmployees()
       .then(({ data }) => {
-        console.log(data);
         setEmployees(data);
       })
       .catch(() => {
@@ -18,9 +20,30 @@ export const Dashboard = () => {
       });
   }, []);
 
-  const handleColumnClick = (columnName: string) => {
+  useEffect(() => {
     // Simulate front-end filtering, however if data is huge we would definitely shift it to back-end
-    console.log(columnName);
+    employees.sort((a: Employee, b: Employee) => {
+      const aProperty = a[filterColumn];
+      const bProperty = b[filterColumn];
+
+      if (aProperty === bProperty) return 0;
+      return aProperty > bProperty ? 1 : -1;
+    });
+
+    setEmployees([...employees]);
+  }, [filterColumn]);
+
+  useEffect(() => {
+    setEmployees([...employees.reverse()]);
+  }, [filterColumnAsc]);
+
+  const handleColumnClick = (columnName: string) => {
+    if (filterColumn === columnName) {
+      setFilterColumnAsc(!filterColumnAsc); // Invert order
+      return;
+    }
+
+    setFilterColumn(columnName);
   }
 
   return (
